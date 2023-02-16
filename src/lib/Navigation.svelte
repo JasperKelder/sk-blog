1c2d82a841c7268a2b998fc5c5f369e08d14baeb
JasperKelder <jasperkelder@gmail.com>
2023-02-16 10:23:46 +0100

    Initial commit

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,6 @@
+<nav>
+	<a href="/">Blog</a>
+	<a href="/register">Register</a>
+	<a href="/login">Login</a>
+	<a href="/admin">Admin</a>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../styles.css';
+	import PageTransition from '$lib/PageTransition.svelte';
+	import { navigating } from '$app/stores';
+	import Navigation from '$lib/Navigation.svelte';
+
+	$: navigate = ($navigating?.from !== $navigating?.to).toString();
+</script>
+
+<Navigation />
+
+<PageTransition trigger={navigate}>
+	<slot />
+</PageTransition>
