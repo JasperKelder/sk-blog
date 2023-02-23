8e6ba3b0f9e02d209a98780fedc0d5d675723248
JasperKelder <jasperkelder@gmail.com>
2023-02-23 12:52:53 +0100

    Tweaks, progressive enhancement and comment start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,28 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import { fade } from 'svelte/transition';
-	import { flip } from 'svelte/animate';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-
-	$: ({ articles } = data);
 </script>
 
 <div class="grid">
 	<div>
 		<h1>Blog</h1>
 
-		{#each articles as article (article.id)}
-			<a
-				href="/{article.id}"
-				in:fade={{ duration: 400 }}
-				out:fade={{ duration: 300 }}
-				animate:flip={{ duration: 600 }}
-			>
+		{#each data.articles as article (article.id)}
+			<a href="/{article.id}">
 				<article>
 					<header>{article.title}</header>
 					<p>
 						{article.content}
 					</p>
-				</article></a
-			>
+				</article>
+			</a>
 		{/each}
 	</div>
 </div>
+
+<style lang="scss">
+	a {
+		text-decoration: none;
+	}
+</style>
